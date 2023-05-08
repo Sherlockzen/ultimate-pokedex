e3cea27608f5c5b280e478b3b4244a67939b4d4d
Hewerton <hewertonlourenco@gmail.com>
2023-05-08 19:53:32 -0300

    Feat: add generations list from the getGenerations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getGenerations } from "../queries";
 
 interface NavPros {
   title: string,
   children: ReactNode,
 }
 function Navbar( { title, children }: NavPros ) {
+  const { status, error, data: generations } = useQuery({
+    queryKey: ['generations'],
+    queryFn: () => getGenerations()
+  })
+
+  console.log(generations?.data.results);
+  
 
   return (
     <div className="drawer">
@@ -31,9 +40,16 @@ function Navbar( { title, children }: NavPros ) {
           <div className="flex-1 px-2 mx-2">{ title }</div>
           <div className="flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost rounded-btn capitalize">Gerações</label>
+              <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                <li><a>Item 1</a></li> 
+                <li><a>Item 2</a></li>
+                {
+                  generations?.data.results.map((elem: {name: string}) => <li key={elem.name}><a>{elem.name}</a></li>)
+                }
+              </ul>
+            </div>
               <li>
                 <a>Navbar Item 2</a>
               </li>
