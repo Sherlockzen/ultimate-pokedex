19f8ea8d264276c61b57e31022560ec3740e6e04
Hewerton <hewertonlourenco@gmail.com>
2023-05-08 21:57:39 -0300

    Feat: infinite list with useInfiniteQuery

--- src/components/ListPokemons.tsx
+++ src/components/ListPokemons.tsx
@@ -1,25 +1,65 @@
-import { axiosGet, getPokemon } from "../queries";
+import { axiosGet } from "../queries";
 import Card from "./Card";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { pokemonsPaginated } from "../queries";
+// import { useEffect } from "react";
+
+interface Iprev {
+    count: number
+    next: string
+    previous: string
+    results: any[]
+  }
 
 const ListPokemons = () => {
-    const { status, error, data: pokemons} = useQuery({
+    // const { status, error, data: pokemons} = useQuery({
+    //     queryKey: ['pokemons'],
+    //     queryFn: () => axiosGet().then((e) => e?.data)
+    // })
+
+    const {
+        data: pokemons,
+        error,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery({
         queryKey: ['pokemons'],
-        queryFn: () => axiosGet().then((e) => e?.data)
+        getNextPageParam: (prevData: Iprev) => prevData?.next,
+        queryFn: ({ pageParam }) => pokemonsPaginated(pageParam) 
     })
+
+    console.log(pokemons?.pages);
+    
 
     if (status === 'loading') return <progress className="progress w-56"></progress>
     if (status === 'error') return <h1>{JSON.stringify(error)}</h1>
 
+    
     return (
+      <>
         <div className={" ListPokes flex flex-wrap gap-10 justify-center"}>
-           {
-            pokemons?.results.map((elem: {name: string, url: string}) => 
+          {/* {
+               pokemons?.results.map((elem: {name: string, url: string}) => 
                 <Card key={elem.name} name={elem.name} url={elem.url} />
-            )
-           } 
+                )
+            }  */}
+
+          {pokemons?.pages
+            .flatMap((data) => data.results)
+            .map((pokemon) => (
+              <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />
+            ))}
         </div>
-    )
+        {hasNextPage && (
+          <button
+            className=" border border-black"
+            onClick={() => fetchNextPage()}
+          >
+            Load more
+          </button>
+        )}
+      </>
+    );
 }
 
 export default ListPokemons;
